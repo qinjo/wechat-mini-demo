9615039916fd4442a1e4444aa8c4f5c790a653da
Vincent <vincent.zhang@accentrix.com>
2024-12-17 18:00:58 +0800

    try get user info

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,1 +1,14 @@
-Page({});
+Page({
+  data: { 
+    name: "",
+    url: '',
+   },
+  buttonHandler(event) {
+    console.log('<===== event', event.detail.userInfo.avatarUrl, '=====>');
+    if (!event.detail.userInfo) return;
+    this.setData({
+      name: event.detail.userInfo.nickName,
+      url: event.detail.userInfo.avatarUrl
+    });
+  },
+});
